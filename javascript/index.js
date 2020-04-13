2967fa4a2636fe9026a90a17d09e371e460240bf
carlijnbruin <carlijn.bruin@hva.nl>
2020-04-13 23:40:29 +0200

    Code wat consistenter gemaakt

--- javascript/index.js
+++ javascript/index.js
@@ -1,16 +1,15 @@
-console.log('Test, JavaScript werkt!');
+console.log("Test, JavaScript werkt!");
 
 
 const myButton = document.getElementById("loveButton");	// global variable
-const myText = document.getElementById("clickText"); 		// global variable
-
-let clicked = 0;																				// global variable
 
 if(myButton){ 																					// alleen uitvoeren als myButton aanwezig is
-myButton.addEventListener('click', function handleClick() {
+	const myText = document.getElementById("clickText");
+	let clicked = 0;
+	myButton.addEventListener('click', function handleClick() {
 		clicked++;
 		myText.innerText = `Je hebt ${clicked} keer op mij geklikt!`;
-	});
+	})
 }
 
 
@@ -18,38 +17,39 @@ myButton.addEventListener('click', function handleClick() {
 const inputDate = document.getElementById("bday");
 
 if (inputDate){																					// indien inputDate op die specifieke html page staat, voer dan de functie uit.
-inputDate.addEventListener('change', function(){				// wanneer een pagina laadt, heet het een event. Event 'change' wordt uitgevoerd in het geval dat de user iets veranderd in de input type.
+inputDate.addEventListener("change", function(){				// wanneer een pagina laadt, heet het een event. Event 'change' wordt uitgevoerd in het geval dat de user iets veranderd in de input type.
 	 let stringDate = "Jouw verjaardag is op: ";
 	 const bdateInput = inputDate.value;									// haalt de waarde die is ingevuld uit de input type date
 	 const bday = +new Date(bdateInput);
 	 stringDate += bdateInput + ", jij bent " + ~~ ((Date.now() - bday) / (31557600000)) + "."; // Hier wordt de leeftijd uitgerekend
-	 const theBday = document.getElementById('resultBday');
+	 const theBday = document.getElementById("resultBday");
 	 theBday.innerHTML = stringDate;											// hier komt het allemaal in theBday (p) te staan in de HTML.
-	});
+	})
 }
 
 
 const resetButton = document.getElementById("resetButton");
-const myFormIndex = document.getElementById("signUpForm");
-const myFormText = document.getElementById("addTextForm")
 
-if (resetButton){																				// indien resetButton op die specifieke html page staat, voer dan de functie uit.
+if (resetButton){
+	const myFormIndex = document.getElementById("signUpForm");
+	const myFormText = document.getElementById("addTextForm");																			// indien resetButton op die specifieke html page staat, voer dan de functie uit.
 	if (myFormIndex){
-resetButton.addEventListener('click', function(){				// reset het formulier
-  myFormIndex.reset();
-	} if (myFormText){
-	myFormText.reset();
+		resetButton.addEventListener("click", function(){
+			myFormIndex.reset();
+		})
+	} else if (myFormText) {
+		resetButton.addEventListener("click", function(){
+			myFormText.reset();
+		})
 	}
-	return;
-	});
 }
 
 
 const alertButton = document.getElementById("alertBox");
 
 if (alertButton){																				// indien alertButton op die specifieke html page staat, voer dan de functie uit.
-alertButton.addEventListener('click', function(){
-	 alert('Weet u zeker dat u alles goed heeft ingevuld? Dan word je profiel nu aangemaakt!')
+alertButton.addEventListener("click", function(){
+	 alert("Weet u zeker dat u alles goed heeft ingevuld? Dan word je profiel nu aangemaakt!");
 	})
 }
 
@@ -58,8 +58,8 @@ alertButton.addEventListener('click', function(){
 const profilePic = document.getElementById("inputProfilePic");
 
 if(profilePic){																					// indien profilePic op die specifieke html page staat, voer dan de functie uit.
-	profilePic.addEventListener('change', function(event){ // wanneer een pagina laadt, heet het een event. Event 'change' wordt uitgevoerd in het geval dat de user iets veranderd in de input type.
-		const image = document.getElementById('output');    // de img tag waar de geuploade afbeelding in gepreviewd wordt.
+	profilePic.addEventListener("change", function(event){ // wanneer een pagina laadt, heet het een event. Event 'change' wordt uitgevoerd in het geval dat de user iets veranderd in de input type.
+		const image = document.getElementById("output");    // de img tag waar de geuploade afbeelding in gepreviewd wordt.
 		image.src = URL.createObjectURL(event.target.files[0]);
 	})
 }
